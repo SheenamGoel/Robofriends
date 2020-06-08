b1c6051c0363d48c0ff44331621f3718d6934833
SheenamGoel <goelsheenam469@gmail.com>
2020-06-08 18:59:44 +0530

    Updates

---
+++ precache-manifest.08da0d799a01acfbe2a112108a430a7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac085ebee9377aa08e5a40ccc184bb0",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "abf1936138076e62458b",
+    "url": "/Robofriends/static/css/main.9d5f6360.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/Robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf1936138076e62458b",
+    "url": "/Robofriends/static/js/main.da718934.chunk.js"
+  },
+  {
+    "revision": "4eac723178dd1eab715f",
+    "url": "/Robofriends/static/js/runtime-main.33ca4b66.js"
+  }
+]);
